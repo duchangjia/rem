0edbe068ed3062bbe5692a18ba535b22ae9106e8
fqlnet <fangqlnet@163.com>
2017-09-19 17:32:27 +0800

    add header

--- Personnel_MS/MS_client/src/components/common/Header.vue
+++ Personnel_MS/MS_client/src/components/common/Header.vue
@@ -1,35 +1,112 @@
 <template>
-    <el-row class="header">
-      <el-col :span="24">后台管理系统</el-col>
-    </el-row> 
+    <div class="header">
+        <span class="homelogo">
+            <img src="../../../static/img/common/home_logo.png" alt="" width="120" height="30" class="">
+        </span>
+        <span class="headertitle">项目管理系统</span>
+        <div class="headerright">
+            <span class="tips">
+                <img src="../../../static/img/common/Group.png" alt="" width="14" height="16" class="">
+                <!-- <span>8</span> -->
+            </span>
+            <span><img src="../../../static/img/common/help.png" alt="" width="14" height="16" class=""></span>
+            <el-dropdown trigger="hover" class="userinfo">
+                <span class="el-dropdown-link"><img src="../../../static/img/common/avatar.png" width="32" height="32" class="useravatar" />您好，{{username}}</span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>基本信息</el-dropdown-item>
+                    <el-dropdown-item>密码修改</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+            <span @click="handleCommand('loginout')">
+                <img src="../../../static/img/common/exit0.png" alt="" width="16" height="18" class="">
+            </span>
+        </div>
+    </div>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-                name: 'ifdp'
-            }
-        },
-        computed:{
-            username(){
-                let username = localStorage.getItem('ms_username');
-                return username ? username : this.name;
-            }
-        },
-        methods:{
-            handleCommand(command) {
-                if(command == 'loginout'){
-                    localStorage.removeItem('ms_username')
-                    this.$router.push('/login');
-                }
+export default {
+    data() {
+        return {
+            name: 'ifdp'
+        }
+    },
+    computed: {
+        username() {
+            let username = localStorage.getItem('ms_username');
+            return username ? username : this.name;
+        }
+    },
+    methods: {
+        handleCommand(command) {
+            var _this = this;
+            if (command == 'loginout') {
+                this.$confirm('确认退出吗?', '提示', {
+                }).then(() => {
+                    localStorage.removeItem('ms_username');
+                    _this.$router.push('/login');
+                }).catch(() => {
+
+                });
             }
         }
     }
+}
 </script>
 
 <style scoped>
-    .header{
-        height: 44px;
-        line-height: 44px;
-    }
+.header {
+    height: 60px;
+    line-height: 60px;
+    color: #cccccc;
+    background: #363D47;
+}
+
+.header .homelogo {
+    padding: 0 20px;
+    border-right: 1px solid #cccccc;
+}
+
+.header .headertitle {
+    font-size: 18px;
+    font-family: "Microsoft YaHei";
+    padding-left: 10px;
+}
+
+.header .headerright {
+    float: right;
+}
+
+.header .headerright>span {
+    display: inline-block;
+    width: 60px;
+    text-align: center;
+    /* background: red; */
+}
+
+.header .headerright>span.tips span {
+    display: inline-block;
+    width: 8px;
+    height: 8px;
+    background: red;
+    border-radius: 50%;
+    position: absolute;
+}
+
+.header .headerright>span:hover,
+.header .headerright .userinfo:hover {
+    cursor: pointer;
+    background: #000000;
+}
+
+.header .headerright .userinfo {
+    padding: 0 20px;
+}
+
+.header .headerright .userinfo.el-dropdown {
+    color: #cccccc;
+}
+.header .headerright .userinfo .useravatar {
+    margin-right: 15px;
+    border-radius: 50%;
+}
 </style>
