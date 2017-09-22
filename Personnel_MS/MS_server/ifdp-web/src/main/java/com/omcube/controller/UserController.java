d0cfc3b6c2a7fb88e891004ca9df73b2c49cbf89
cunevJin <934244186@qq.com>
2017-09-22 10:02:19 +0800

    test

--- Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/controller/UserController.java
+++ Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.omcube.controller;
 
 import com.omcube.model.po.SysUserPO;
+import com.omcube.util.JSONResultUtil;
+import com.omcube.util.SpringUtil;
 import com.omcube.model.mapper.SysUserMapper;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,13 +21,34 @@ import org.springframework.web.bind.annotation.RestController;
 @CacheConfig(cacheNames = "users")
 public class UserController {
 
+	protected final Log logger = LogFactory.getLog(getClass());
+	
     @Autowired
     private SysUserMapper userMapper;
 
-    @RequestMapping("/{userNo}")
+    @RequestMapping(value = "/queryUser", method = RequestMethod.GET)
     @Cacheable
-    public SysUserPO getUser(@PathVariable String userNo) {
-        return userMapper.getUserByUserNo(userNo);
+    public Object queryUser(@RequestParam String userNo,@RequestParam String userName) {
+    	/*logger.info(String.format("the request pragram userNo:%s,userName:%s", userNo,userName));
+    	//校验参数，至少一个不为空
+    	if(StringUtils.isEmpty(userNo) && StringUtils.isEmpty(userName))
+    	{
+    		logger.error("the request params userNo and userName is null");
+    		return JSONResultUtil.setError("F00002", "the request params userNo and userName is null");
+    	}
+    	if (userMapper == null)
+    	{
+    		userMapper = SpringUtil.getBean(SysUserMapper.class);
+    	}
+    	SysUserPO sysUserPo = userMapper.queryUser(userNo,userName);
+    	logger.info(sysUserPo);*/
+        return JSONResultUtil.setSuccess( userMapper.queryUser("1","jinx"));
     }
 
+    @RequestMapping(value = "/queryUserByName", method = RequestMethod.GET)
+    @Cacheable
+    public Object queryUserByName(@RequestParam String userName)
+    {
+    	return JSONResultUtil.setSuccess(userMapper.getUserByUsername(userName));
+    }
 }
--- Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/model/mapper/SysUserMapper.java
+++ Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/model/mapper/SysUserMapper.java
@@ -2,12 +2,23 @@ package com.omcube.model.mapper;
 
 import com.omcube.model.po.SysUserPO;
 import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.jdbc.SQL;
+import org.springframework.util.StringUtils;
 
 @Mapper
 public interface SysUserMapper {
-    @Select("select * from IFDP_SYS_USER_BASE where user_no= #{userNo}")
-    @ResultMap("SysUserMap")// 公共的使用@ResultMap
-    SysUserPO getUserByUserNo(String userNo);
+    //@SelectProvider(type=CreateUserSql.class, method = "queryUserSql")
+    //@ResultMap("SysUserMap")// 公共的使用@ResultMap
+	@Select("select * from IFDP_SYS_USER_BASE where user_no=#{userNo} and log_name=#{userName}")
+    @Results({
+        @Result(id = true, column = "user_no", property = "userNo"),
+        @Result(column = "log_name", property = "userName"),
+        @Result(column = "pswd", property = "password"),
+        @Result(column = "mobile_tel", property = "mobileTEL"),
+        @Result(column = "email", property = "email"),
+        @Result(column = "user_no", property = "roles", many = @Many(select = "com.omcube.model.mapper.SysRoleMapper.getByUserNo"))
+    })
+    SysUserPO queryUser(String userNo, String userName);
 
     @Select("select * from IFDP_SYS_USER_BASE where log_name=#{userName}")
     @Results({
@@ -20,4 +31,21 @@ public interface SysUserMapper {
     })// 单个的使用@Results
     SysUserPO getUserByUsername(String userName);
 
+    class CreateUserSql{
+    	public String queryUserSql(final String userNo,final String userName)
+    	{
+    		
+    		SQL sql = new SQL().SELECT("*").FROM("IFDP_SYS_USER_BASE");
+    		if (!StringUtils.isEmpty(userNo))
+    		{
+    			sql.WHERE("user_no = #{userNo}");
+    		}
+    		if (!StringUtils.isEmpty(userName))
+    		{
+    			sql.WHERE("log_name = #{userName}");
+    		}
+    		return sql.toString();
+    	}
+    	
+    }
 }
