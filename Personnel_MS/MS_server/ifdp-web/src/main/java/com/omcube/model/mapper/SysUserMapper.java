1261364120df65e433e10448ca259fa56bd79a54
cunevJin <934244186@qq.com>
2017-09-28 21:24:56 +0800

    查询用户

--- Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/model/mapper/SysUserMapper.java
+++ Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/model/mapper/SysUserMapper.java
@@ -1,6 +1,7 @@
 package com.omcube.model.mapper;
 
-import com.omcube.model.po.SysUserPO;
+import com.omcube.model.request.QueryUserRequest;
+import com.omcube.model.response.QueryUserInfoResponse;
 
 import java.util.List;
 
@@ -8,26 +9,7 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface SysUserMapper {
    
-    SysUserPO queryUser(@Param("userNo") String userNo, @Param("userName") String userName);
+    List<QueryUserInfoResponse> queryUser(QueryUserRequest queryUserReq);
 
-    @Select("select * from IFDP_SYS_USER_BASE where log_name=#{userName}")
-    @Results({
-            @Result(id = true, column = "user_no", property = "userNo"),
-            @Result(column = "log_name", property = "userName"),
-            @Result(column = "pswd", property = "password"),
-            @Result(column = "mobile_tel", property = "mobileTEL"),
-            @Result(column = "email", property = "email"),
-            @Result(column = "user_no", property = "roles", many = @Many(select = "com.omcube.model.mapper.SysRoleMapper.getByUserNo"))
-    })// 单个的使用@Results
-    SysUserPO getUserByUsername(String userName);
-
-    @Select("select * from IFDP_SYS_USER_BASE")
-    @Results({
-            @Result(id = true, column = "user_no", property = "userNo"),
-            @Result(column = "log_name", property = "userName"),
-            @Result(column = "pswd", property = "password"),
-            @Result(column = "mobile_tel", property = "mobileTEL"),
-            @Result(column = "email", property = "email"),
-    })
-    List<SysUserPO> getUserAll();
+    List<QueryUserInfoResponse> queryUserLoad(@Param("uid") String uid,@Param("userNo") String userNo);
 }
