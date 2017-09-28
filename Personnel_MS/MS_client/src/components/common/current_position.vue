688f78e3909b67af8833ca88ca636d14835ffc73
chenyongjian <80729453@qq.com>
2017-09-28 19:01:36 +0800

    修改登录loading的样式

--- Personnel_MS/MS_client/src/components/common/current_position.vue
+++ Personnel_MS/MS_client/src/components/common/current_position.vue
@@ -11,7 +11,7 @@
                     <el-breadcrumb-item v-if="wuji" :to="{ path: links[4] }" class="test1" >{{wuji}}</el-breadcrumb-item>
                 </el-breadcrumb>
             </el-col>
-            <el-col :span="2" v-show="breadItemLength>2">
+            <el-col :span="2" v-show="breadItemLength>1">
                 <img src="../../../static/img/common/back.png" alt="pic" width="19" height="12" class="pic" @click="jump">
             </el-col>
         </el-row>
