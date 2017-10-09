f50b9aef013a6dc3b70df475cec79e13ce98a482
wurongxing <rongxing.wu@pactera.com>
2017-10-09 14:44:53 +0800

    update code

--- Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/controller/OrganBillInfoController.java
+++ Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/controller/OrganBillInfoController.java
@@ -20,8 +20,15 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.omcube.model.mapper.EpOrganBillInfoMapper;
 import com.omcube.model.po.EpOrganBillInfoPO;
+import com.omcube.service.OrganBillInfoService;
 import com.omcube.util.JSONResultUtil;
 
+/**
+ * 公司开票信息维护
+ * 
+ * @author rongxing
+ * @version 1.0
+ */
 
 @RestController
 @RequestMapping(value = "/organBillInfo")
@@ -30,14 +37,23 @@ public class OrganBillInfoController {
 
 	protected final Log logger = LogFactory.getLog(getClass());
 	@Autowired
-	private EpOrganBillInfoMapper billInfoMapper;
+	private OrganBillInfoService billInfoService;
 
-	// 新增开票信息
-	@RequestMapping(value = "/addOrganBillInfo/{uId}", method = RequestMethod.GET)
+	/**
+	 * 新增开票信息
+	 * 
+	 * @param uId
+	 * @return
+	 * @throws ParseException
+	 */
+	@RequestMapping(value = "/addOrganBillInfo", method = RequestMethod.GET)
 	@Cacheable
-	public Object addOrganBillInfo(@PathVariable String uId) throws ParseException {
-		EpOrganBillInfoPO billInfoPO = new EpOrganBillInfoPO();
-		billInfoPO.setuId(uId);
+	public Object addOrganBillInfo(EpOrganBillInfoPO billInfoPO) throws ParseException {
+		if (billInfoPO == null) {
+			logger.error("the request body is null");
+			return JSONResultUtil.setError("F0002", "the request body is null");
+		}
+		billInfoPO.setuId("0001");
 		billInfoPO.setOrganNo("10008");
 		billInfoPO.setOrganName("");
 		billInfoPO.setOrganTaxNo("");
@@ -47,19 +63,22 @@ public class OrganBillInfoController {
 		billInfoPO.setOrganAddr("福田");
 		billInfoPO.setCreatedBy("wrx");
 		billInfoPO.setUpdatedBy("wrx");
-		if (StringUtils.isEmpty(billInfoPO.getOrganName()) || StringUtils.isEmpty(billInfoPO.getOrganTaxNo())) {
-			return JSONResultUtil.setError("F00002", "the request params userNo and userName is null");
-		}
-		billInfoMapper.addOrganBillInfo(billInfoPO);
+		
+		billInfoService.addOrganBillInfo(billInfoPO);
 		logger.info("uid:" + billInfoPO.getuId() + "organName" + billInfoPO.getOrganName() + "新增公司信息成功");
 		return JSONResultUtil.setSuccess();
 	}
 
-	// 修改开票信息
+	/**
+	 * 修改开票信息
+	 * 
+	 * @return
+	 * @throws ParseException
+	 */
 	@RequestMapping(value = "/updateOrganBillInfo", method = RequestMethod.GET)
 	@Cacheable
-	public Object updateOrganBillInfo() throws ParseException {
-		EpOrganBillInfoPO billInfoPO = new EpOrganBillInfoPO();
+	public Object updateOrganBillInfo(EpOrganBillInfoPO billInfoPO) throws ParseException {
+		
 		billInfoPO.setuId("0001");
 		billInfoPO.setOrganNo("10001");
 		billInfoPO.setOrganName("深圳平安0001");
@@ -69,25 +88,51 @@ public class OrganBillInfoController {
 		billInfoPO.setOrganAcctname("平安科技");
 		billInfoPO.setOrganAddr("深圳");
 		billInfoPO.setUpdatedBy("wurongxing");
-		billInfoMapper.updateOrganBillInfo(billInfoPO);
+		
+		billInfoService.updateOrganBillInfo(billInfoPO);
 		logger.info("uid:" + billInfoPO.getuId() + "organName" + billInfoPO.getOrganName() + "修改公司信息成功");
 		return JSONResultUtil.setSuccess(billInfoPO);
 	}
 
-	// 查询公司开票信息列表
+	/**
+	 * 查询公司开票信息列表
+	 * 
+	 * @param uId
+	 * @param request
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 */
 	@RequestMapping(value = "/queryBillInfoList/{uId}", method = RequestMethod.GET)
 	@Cacheable
-	public Object quetyBillInfoList(@PathVariable String uId, HttpServletRequest request, Integer pageNum,
-			Integer pageSize) {
+	public Object quetyBillInfoList(@PathVariable(value = "uId", required = true) String uId,
+			HttpServletRequest request, Integer pageNum, Integer pageSize) {
+
+		if(StringUtils.isEmpty(uId)){
+			logger.error("the request param uId is null");
+			return JSONResultUtil.setError("F0002", "the request param uId is null");
+		}
+		
 		pageNum = pageNum == null ? 1 : pageNum;
 		pageSize = pageSize == null ? 5 : pageSize;
 		PageHelper.startPage(pageNum, pageSize, true);
-		List<EpOrganBillInfoPO> billInfoPOList = billInfoMapper.queryBillInfoList(uId);
+
+		List<EpOrganBillInfoPO> billInfoPOList = billInfoService.queryBillInfoList(uId);
 		PageInfo<EpOrganBillInfoPO> pageInfo = new PageInfo<EpOrganBillInfoPO>(billInfoPOList);
+		
 		return JSONResultUtil.setSuccess(pageInfo);
 	}
 
-	// 根据名称模糊查询开票信息
+	/**
+	 * 根据名称模糊查询开票信息
+	 * 
+	 * @param uId
+	 * @param organName
+	 * @param request
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 */
 	@RequestMapping(value = "/queryBillInfoByName/{uId}&{organName}", method = RequestMethod.GET)
 	@Cacheable
 	public Object queryBillInfoByName(@PathVariable String uId, @PathVariable String organName,
@@ -95,7 +140,7 @@ public class OrganBillInfoController {
 		pageNum = pageNum == null ? 1 : pageNum;
 		pageSize = pageSize == null ? 5 : pageSize;
 		PageHelper.startPage(pageNum, pageSize, true);
-		List<EpOrganBillInfoPO> billInfoPOList = billInfoMapper.queryBillInfoByName(uId, organName);
+		List<EpOrganBillInfoPO> billInfoPOList = billInfoService.queryBillInfoByName(uId, organName);
 		PageInfo<EpOrganBillInfoPO> pageInfo = new PageInfo<EpOrganBillInfoPO>(billInfoPOList);
 		return JSONResultUtil.setSuccess(pageInfo);
 	}
