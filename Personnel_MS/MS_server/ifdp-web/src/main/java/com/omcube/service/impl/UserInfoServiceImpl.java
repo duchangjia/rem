31cc1c9d018f81e7ed7fa53f15376007ea5e5fef
cunevJin <934244186@qq.com>
2017-10-10 10:54:35 +0800

    代码整改

--- Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/service/impl/UserInfoServiceImpl.java
+++ Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/service/impl/UserInfoServiceImpl.java
@@ -1,15 +1,19 @@
 package com.omcube.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.omcube.model.mapper.SysUserMapper;
+import com.omcube.model.request.QueryUserRequest;
 import com.omcube.model.request.UpdateUserInfoRequest;
-import com.omcube.service.UserInfoUpdateService;
+import com.omcube.model.response.QueryUserInfoResponse;
+import com.omcube.service.UserInfoService;
 
 @Service
-public class UserInfoUpdateServiceImpl implements UserInfoUpdateService {
+public class UserInfoServiceImpl implements UserInfoService {
 
     @Autowired
     private SysUserMapper userMapper;
@@ -24,6 +28,18 @@ public class UserInfoUpdateServiceImpl implements UserInfoUpdateService {
 	//更用户角色关联表
 	userMapper.updteUserRoleInfo(updateUserReq);
 
+    }
+
+    @Override
+    public List<QueryUserInfoResponse> queryUser(QueryUserRequest queryUserReq)
+    {
+	return userMapper.queryUser(queryUserReq);
+    }
+
+    @Override
+    public List<QueryUserInfoResponse> queryUserLoad(String uid, String userNo)
+    {
+	return userMapper.queryUserLoad(uid,userNo);
     }
 
 }
