7a55fac2a8b6db4732c8a290f163052d0b58aa77
fqlnet <fangqlnet@163.com>
2017-10-12 10:44:23 +0800

    m

--- Personnel_MS/MS_client/src/components/pages/system_Mgmt/query_role.vue
+++ Personnel_MS/MS_client/src/components/pages/system_Mgmt/query_role.vue
@@ -54,7 +54,7 @@ export default {
         self.$axios.get('iemrole/role/queryRoleList', { params })
             .then(function(res) {
                 console.log(res);
-                self.roleListInfo = res.data.data.model;
+                self.roleListInfo = res.data.data.models;
                 self.pageNum = Number(res.data.config.pageNum);
                 self.pageSize = Number(res.data.config.pageSize);
                 self.totalRows = Number(res.data.data.total);
