cf95b0a940bbdea134048e6b77b8bf3319503927
程龙 <tschenglong@163.com>
2017-10-16 16:47:22 +0800

    机构CCC管理

--- Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/model/mapper/OrganCCCManagementMapper.java
+++ Personnel_MS/MS_server/ifdp-web/src/main/java/com/omcube/model/mapper/OrganCCCManagementMapper.java
@@ -2,11 +2,8 @@ package com.omcube.model.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.omcube.model.po.OrganCCCManagemenPO;
 
