a014fb27c577c7fc13ca04a91a10b425449e42a6
zhouhongkang <zhouhongkangz@qq.com>
2017-10-20 19:49:14 +0800

    调薪管理删除调薪用户信息、修改调薪用户信息

--- Personnel_MS/MS_server/ifdp-web/src/test/java/com/omcube/controller/TaxRateGroupControllerTest.java
+++ Personnel_MS/MS_server/ifdp-web/src/test/java/com/omcube/controller/TaxRateGroupControllerTest.java
@@ -19,56 +19,53 @@ import org.springframework.web.context.WebApplicationContext;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TaxRateGroupControllerTest {
-    @Autowired
-    private WebApplicationContext wac;
+	@Autowired
+	private WebApplicationContext wac;
 
-    private MockMvc mockMvc;
-    
-    @Before
-    public void setup()
-    {
-	mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-    }
-    
-    // 新增个人所得税税率组
-    @Test
-    public void testAddTaxRateGroup() throws Exception{
-	String result = mockMvc.perform(post("/taxRateGroup/insertTaxRateGroup")
-		.param("uid", "001")
-		.param("groupId", "001016")
-		.param("groupNo", "小黄")
-		.param("startTime", "2017-08-18")
-		.param("isDelete", "1")
-		.contentType(MediaType.APPLICATION_JSON_UTF8))
-		.andExpect(status().isOk()).andReturn().getResponse()
-		.getContentAsString();
-	System.out.println(result);
-    }
+	private MockMvc mockMvc;
 
-    // 根据主键（组ID）删除个人所得税税率组
-    @Test
-    public void testDeleteTaxRateGroup() throws Exception
-    {
-	String result = mockMvc.perform(delete("/taxRateGroup/deleteTaxRateGroup")
-		.param("uid", "001")
-		.param("groupId", "001007")
-		.contentType(MediaType.APPLICATION_JSON_UTF8))
-		.andExpect(status().isOk()).andReturn().getResponse()
-		.getContentAsString();
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
 
-	System.out.println(result);
-    }
-    
-    // 查询所有个人所得税税率组
-    @Test
-    public void testSelectTaxRateGroup() throws Exception
-    {
-	String result = mockMvc.perform(get("/taxRateGroup/selectTaxRateGroup")
-		.param("uid", "001")
-		.contentType(MediaType.APPLICATION_JSON_UTF8))
-		.andExpect(status().isOk()).andReturn().getResponse()
-		.getContentAsString();
-        System.out.println(result);
-    }
-    
+	// 新增个人所得税税率组
+	@Test
+	public void testAddTaxRateGroup() throws Exception {
+		String result = mockMvc
+				.perform(post("/taxRateGroup/insertTaxRateGroup")
+						.param("uid", "001").param("groupId", "001016")
+						.param("groupNo", "小黄").param("startTime", "2017-08-18")
+						.param("isDelete", "1")
+						.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString();
+		System.out.println(result);
+	}
+
+	// 根据主键（组ID）删除个人所得税税率组
+	@Test
+	public void testDeleteTaxRateGroup() throws Exception {
+		String result = mockMvc
+				.perform(delete("/taxRateGroup/deleteTaxRateGroup")
+						.param("uid", "001").param("groupId", "001007")
+						.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString();
+
+		System.out.println(result);
+	}
+
+	// 查询所有个人所得税税率组
+	@Test
+	public void testSelectTaxRateGroup() throws Exception {
+		String result = mockMvc
+				.perform(get("/taxRateGroup/selectTaxRateGroup")
+						.param("uid", "001")
+						.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString();
+		System.out.println(result);
+	}
+
 }
