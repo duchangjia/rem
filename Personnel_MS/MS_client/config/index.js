d366317ae6233dd7cd510eba645775ae552dbf1e
chenyongjian <80729453@qq.com>
2017-10-20 11:22:50 +0800

    完成登录验证码

--- Personnel_MS/MS_client/config/index.js
+++ Personnel_MS/MS_client/config/index.js
@@ -37,32 +37,21 @@ module.exports = {
               '^/ifdp': ''
           },
       },
-      // '/iem_hrm': {
-      //     target: 'http://10.0.0.41:8888/',
-      //     // target: 'http://10.0.0.34:8888/',
-      //     changeOrigin: true,
-      //     secure: false,
-      //     pathRewrite: {
-      //         '^/iem_hrm': ''
-      //     }
-      // },
-//    '/iemrole': {
-//      target: 'http://192.168.1.105:8888/',
-//      changeOrigin: true,
-//      secure: false,
-//      pathRewrite: {
-//        '^/iemrole': ''
-//      }
-//    }
-//    '/iem_hrm': {
-//        target: 'http://10.0.0.41:8888/',
-//        // target: 'http://10.0.0.34:8888/',
-//        changeOrigin: true,
-//        secure: false,
-//        pathRewrite: {
-//            '^/iem_hrm': ''
-//        }
-//    }
+        '/api': {
+            target: 'http://10.0.0.242:5555/',
+            // target: 'http://10.0.0.34:8888/',
+            changeOrigin: true,
+            secure: false,
+        },
+      '/iem_hrm': {
+          target: 'http://10.0.0.242:8888/',
+          // target: 'http://10.0.0.34:8888/',
+          changeOrigin: true,
+          secure: false,
+          pathRewrite: {
+              '^/iem_hrm': ''
+          }
+      },
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
