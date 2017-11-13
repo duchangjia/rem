564882c0299165c8823e1b193c4a822456a70c7a
xiaoyufeng <yufeng.xiao@pactera.com>
2017-11-13 17:48:05 +0800

    管理

--- Personnel_MS/MS_client/src/components/pages/asset_Mgmt/assetUseMgmt/query_assetUse.vue
+++ Personnel_MS/MS_client/src/components/pages/asset_Mgmt/assetUseMgmt/query_assetUse.vue
@@ -88,7 +88,7 @@
 
 <script type='text/ecmascript-6'>
 import current from "../../../common/current_position.vue";
-import current from "../../../common/current_position.vue";
+import messageBox from "../../../common/messageBox-components.vue";
 import moment from "moment";
 export default {
   data() {
@@ -136,10 +136,7 @@ export default {
   },
   methods: {
     handleSizeChange() {},
-    handleCurrentChange(val) {
-        this..pageNum = val;
-        this.search();
-    },
+    handleCurrentChange() {},
     assetTypeFormatter(row, column) {
     return row.assetType == "01"
         ? "全部"
@@ -163,20 +160,6 @@ export default {
             :applyType=="06"
             ?"盘亏":"异常";
     },
-    if (self.$route.params) {
-      self.searchInfo.assetNo = self.$route.params.assetNo;
-      self.searchInfo.applyUserNo = self.$route.params.applyUserNo;
-      self.search();
-    } else {
-     	self.search();
-        .catch(e => {
-          console.log(e);
-        });
-    }
-  },
-  methods: {
-    handleSizeChange() {},
-    handleCurrentChange() {},
     search() {
       let self = this;
       let params = {};
@@ -276,7 +259,8 @@ export default {
    
   },
   components: {
-    current
+    current,
+    messageBox
   }
 };
 </script>
@@ -293,9 +277,7 @@ export default {
 }
 .assetUse_query {
     padding: 0 0 20px 20px;
-    // overflow: hidden;
     position: relative;
-
     .test {
         padding-left: 10px;
     }
@@ -377,6 +359,7 @@ export default {
                 border: 1px solid #ff9900;
             }
         }
+
         .el-table .cell, .el-table th>div{
             padding-left: 10px;
             padding-right: 10px;
@@ -386,67 +369,9 @@ export default {
             cursor: pointer;
             text-decoration: underline;
         }
-      }
-   
-     
-   
-    },
-    link(applyNo) {
-      this.$router.push({ name: "detail_assetUse", query: { applyNo } });
-    },
-    edit(applyNo) {
-      this.$router.push({ name: "edit_assetUse", query: { applyNo } });
-    },
-    del(value) {
-      let self = this;
-      this.$axios
-        .get("/iem_hrm/assetUse/delAssUse/" + value)
-        .then(res => {
-          let result = res.data.retMsg;
-          if ("操作成功" === result) {
-            self.$message({
-              message: "删除成功",
-              type: "success"
-            });
-            self.$axios
-              .get("/iem_hrm/assetUse/queryAssUseList")
-              .then(res => {
-                self.table.td = res.data.data.list;
-                self.fenye.total = res.data.data.total;
-                self.fenye.pageSize = res.data.data.pageSize;
-                self.fenye.pageNum = res.data.data.pageNum;
-              })
-              .catch(e => {
-                console.log(e);
-              });
-          } else {
-            self.$message({
-              message: result,
-              type: "error"
-            });
-          }
-        })
-        .catch(e => {
-          console.log(e);
-        });
-    },
-    add() {
-      this.$router.push("add_assetUse");
-    }
-  },
-  filters: {
-    formatDate1(time) {
-      return moment(time).format("YYYY-MM-DD");
-    },
-    formatDate(time) {
-      return moment(time).format("YYYY-MM-DD hh:mm:ss");
-    }
-   },
-  components: {
-    current
-  }
-};
-</script>
+      }  
+}
+</style>
 
 <style lang='stylus' rel='stylesheet/stylus'>
 	.el-pagination {
@@ -486,151 +411,151 @@ export default {
                 text-indent: 0;
             }
         }
-  },
-.assetUse_query {
-    padding: 0 0 20px 20px;
-    overflow: hidden;
-    position: relative;
-
-    .test {
-        padding-left: 10px;
     }
+// .assetUse_query {
+//     padding: 0 0 20px 20px;
+//     overflow: hidden;
+//     position: relative;
 
-    .content-wrapper {
-        background: #fff;
-        padding-left: 20px;
-        padding-right: 20px;
-        height: 746px;
+//     .test {
+//         padding-left: 10px;
+//     }
 
-        .title {
-            font-family: PingFangSC-Regular;
-            font-size: 16px;
-            color: #333333;
-            letter-spacing: 0;
-            height: 80px;
-            line-height: 80px;
-            border-bottom: 1px solid #f4f4f4;
-            position: relative;
+//     .content-wrapper {
+//         background: #fff;
+//         padding-left: 20px;
+//         padding-right: 20px;
+//         height: 746px;
 
-            .text {
-                border-bottom: 2px solid black;
-                display: inline-block;
-                height: 80px;
-            }
+//         .title {
+//             font-family: PingFangSC-Regular;
+//             font-size: 16px;
+//             color: #333333;
+//             letter-spacing: 0;
+//             height: 80px;
+//             line-height: 80px;
+//             border-bottom: 1px solid #f4f4f4;
+//             position: relative;
 
-            .add {
-                width: 120px;
-                height: 40px;
-                background: #FF9900;
-                border: 1px solid #FF9900;
-                outline: none;
-                font-family: PingFangSC-Regular;
-                font-size: 14px;
-                color: #FFFFFF;
-                line-height: 40px;
-                text-align: center;
-                position: absolute;
-                right: 0px;
-                bottom: 20px;
-            }
-        }
+//             .text {
+//                 border-bottom: 2px solid black;
+//                 display: inline-block;
+//                 height: 80px;
+//             }
 
-        .content {
-            padding-top: 42px;
-            overflow: hidden;
+//             .add {
+//                 width: 120px;
+//                 height: 40px;
+//                 background: #FF9900;
+//                 border: 1px solid #FF9900;
+//                 outline: none;
+//                 font-family: PingFangSC-Regular;
+//                 font-size: 14px;
+//                 color: #FFFFFF;
+//                 line-height: 40px;
+//                 text-align: center;
+//                 position: absolute;
+//                 right: 0px;
+//                 bottom: 20px;
+//             }
+//         }
 
-            .search {
-                overflow: hidden;
-                text-align: center;
+//         .content {
+//             padding-top: 42px;
+//             overflow: hidden;
 
-                .text {
-                    color: #999;
-                    font-size: 14px;
-                    margin-right: 10px;
-                }
+//             .search {
+//                 overflow: hidden;
+//                 text-align: center;
 
-                .el-input, .el-select {
-                    width: 200px;
+//                 .text {
+//                     color: #999;
+//                     font-size: 14px;
+//                     margin-right: 10px;
+//                 }
 
-                    .el-input__inner {
-                        width: 100%;
-                    }
+//                 .el-input, .el-select {
+//                     width: 200px;
 
-                    .el-input__inner:hover {
-                        border-color: #f90;
-                    }
-                }
+//                     .el-input__inner {
+//                         width: 100%;
+//                     }
 
-                .toolBtn {
-                    border-radius: 0;
-                    background: #FF9900;
-                    border: none;
-                    font-family: PingFangSC-Regular;
-                    font-size: 14px;
-                    color: #fff;
-                    margin: 40px auto 0 auto;
-                }
-            }
+//                     .el-input__inner:hover {
+//                         border-color: #f90;
+//                     }
+//                 }
 
-            .el-icon-delete2, .el-icon-edit {
-                color: #ff9900;
-                cursor: pointer;
-                text-decoration: underline;
-            }
-        }
-    }
+//                 .toolBtn {
+//                     border-radius: 0;
+//                     background: #FF9900;
+//                     border: none;
+//                     font-family: PingFangSC-Regular;
+//                     font-size: 14px;
+//                     color: #fff;
+//                     margin: 40px auto 0 auto;
+//                 }
+//             }
 
-    .el-pagination {
-        position: absolute;
-        right: 45px;
-        bottom: 40px;
+//             .el-icon-delete2, .el-icon-edit {
+//                 color: #ff9900;
+//                 cursor: pointer;
+//                 text-decoration: underline;
+//             }
+//         }
+//     }
 
-        .el-pagination__total {
-            height: 24px;
-        }
+//     .el-pagination {
+//         position: absolute;
+//         right: 45px;
+//         bottom: 40px;
 
-        .btn-prev, .el-pagination__jump, .btn-next {
-            height: 24px;
-            width: 24px;
-            line-height: 24px;
-        }
+//         .el-pagination__total {
+//             height: 24px;
+//         }
 
-        .el-pager li {
-            width: 24px;
-            height: 24px;
-            line-height: 24px;
-        }
+//         .btn-prev, .el-pagination__jump, .btn-next {
+//             height: 24px;
+//             width: 24px;
+//             line-height: 24px;
+//         }
 
-        .el-pager li.active {
-            background: #ff9900;
-            border: 1px solid #ff9900;
-        }
+//         .el-pager li {
+//             width: 24px;
+//             height: 24px;
+//             line-height: 24px;
+//         }
 
-        .el-pager li:hover, button:hover {
-            color: #ff9900;
-        }
+//         .el-pager li.active {
+//             background: #ff9900;
+//             border: 1px solid #ff9900;
+//         }
 
-        .el-pagination__jump {
-            .el-pagination__editor {
-                width: 24px;
-                height: 24px;
-                line-height: 24px;
-                margin: 0 3px;
-                text-indent: 0;
-            }
-        }
+//         .el-pager li:hover, button:hover {
+//             color: #ff9900;
+//         }
+
+//         .el-pagination__jump {
+//             .el-pagination__editor {
+//                 width: 24px;
+//                 height: 24px;
+//                 line-height: 24px;
+//                 margin: 0 3px;
+//                 text-indent: 0;
+//             }
+//         }
 
 
-        .el-pagination__editor:focus {
-            outline: none;
-            border-color: #ff9900;
-        }
-    }
+//         .el-pagination__editor:focus {
+//             outline: none;
+//             border-color: #ff9900;
+//         }
+//     }
 
-    _:-ms-lang(x), td {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-}
+//     _:-ms-lang(x), td {
+//         display: flex;
+//         align-items: center;
+//         justify-content: center;
+//     }
+// }
 </style>
