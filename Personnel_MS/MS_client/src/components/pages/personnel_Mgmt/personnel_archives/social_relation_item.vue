813466a10665dcdea96bb3db760c99244131e363
chenyongjian <80729453@qq.com>
2017-11-23 12:18:05 +0800

    n

--- Personnel_MS/MS_client/src/components/pages/personnel_Mgmt/personnel_archives/social_relation_item.vue
+++ Personnel_MS/MS_client/src/components/pages/personnel_Mgmt/personnel_archives/social_relation_item.vue
@@ -3,7 +3,8 @@
         <div :class="{'bg_color':!ruleFrom.isShowEdit,'bg_color2':ruleFrom.isShowEdit}">
             <el-form :model="ruleFrom" :rules="rules" :ref="`ruleFrom${relationNum}`" label-width="100px">
                 <div class="title">
-                    <span>关系人{{relationNumber}}</span><i :class="{'el-icon-close':!ruleFrom.isShowEdit,'el-icon-edit':ruleFrom.isShowEdit}" @click="delOrEdit(ruleFrom.isShowEdit,relationNum)"></i>
+                    <span v-show="false">关系人{{relationNumber}}</span><i :class="{'el-icon-close':!ruleFrom.isShowEdit,'el-icon-edit':ruleFrom.isShowEdit}" @click="delOrEdit(ruleFrom.isShowEdit,relationNum)"></i>
+                    <span v-show="false">关系人{{relationNumber}}</span><i :class="{'el-icon-close':!ruleFrom.isShowEdit,'el-icon-edit':ruleFrom.isShowEdit}" @click="delOrEdit(ruleFrom.isShowEdit,relationNum)"></i>
                 </div>
                 <el-col :span="8">
                     <el-form-item label="姓名" prop="contactName">
@@ -111,9 +112,8 @@
                     })
                 }
                 if(isShow) {
-                    this.isShowEdit = !this.isShowEdit
+                    this.ruleFrom.isShowEdit = !isShow
                 }
-
             },
         }
     }
