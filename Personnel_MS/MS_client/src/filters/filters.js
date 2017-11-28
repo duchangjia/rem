df2dc7a58e928937a4c81ddbe0fb56bccaec26d1
xiaoyufeng <yufeng.xiao@pactera.com>
2017-11-28 09:55:53 +0800

    薪资查询

--- Personnel_MS/MS_client/src/filters/filters.js
+++ Personnel_MS/MS_client/src/filters/filters.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-Vue.filter('money-filter', function (value) {
+Vue.filter('moneyFilter', function (value) {
     // var count = value.toFixed(2);
+    if (!value) return '0.00'
     var count = value+'kkk';
     return count;
 });
-var m  = Vue.filter('money-filter')
+// var m  = Vue.filter('money-filter')
