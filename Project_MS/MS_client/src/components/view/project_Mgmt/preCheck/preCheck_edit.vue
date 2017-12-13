4686cebabb731aae8bf21065b21679dfa9a78a8f
xiaoyufeng <yufeng.xiao@pactera.com>
2017-12-13 20:04:18 +0800

    项目管理-项目一览

--- Project_MS/MS_client/src/components/view/project_Mgmt/preCheck/preCheck_edit.vue
+++ Project_MS/MS_client/src/components/view/project_Mgmt/preCheck/preCheck_edit.vue
@@ -10,22 +10,22 @@
                 <el-form label-width="140px" :inline="true">
                     <el-col :sm="24" :md="12">
                         <el-form-item label="机会号">
-                                <el-input :disabled="true"></el-input>
+                                <el-input :disabled="true" v-model="salesInfo.oppoNo"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="项目名称">
-                                <el-input></el-input>
+                                <el-input v-model="salesInfo.projName"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="项目编号">
-                                <el-input></el-input>
+                                <el-input v-model="salesInfo.projNo"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
-                        <el-form-item label="客户">
-                            <el-select>
+                        <el-form-item label="客户" >
+                            <el-select v-model="salesInfo.coocustNo">
                                 <el-option label="客户1" value="01"></el-option>
                                 <el-option label="客户2" value="02"></el-option>
                                 <el-option label="客户3" value="03"></el-option>
@@ -34,7 +34,7 @@
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="服务管理模式">
-                            <el-select>
+                            <el-select v-model="salesInfo.projSrvMngmode">
                                 <el-option label="项目外包" value="01"></el-option>
                                 <el-option label="人力外包" value="02"></el-option>
                                 <el-option label="解决方案" value="03"></el-option>
@@ -44,7 +44,7 @@
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="项目类型">
-                            <el-select>
+                            <el-select v-model="salesInfo.projType">
                                 <el-option label="应用系统开发" value="01"></el-option>
                                 <el-option label="软件产品开发" value="02"></el-option>
                                 <el-option label="应用维护升级" value="03"></el-option>
@@ -57,7 +57,7 @@
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="收入确认类型">
-                            <el-select>
+                            <el-select v-model="salesInfo.projIncmConfim">
                                 <el-option label="外包" value="01"></el-option>
                                 <el-option label="固定金额" value="02"></el-option>
                             </el-select>
@@ -65,12 +65,12 @@
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="预计合同金额">
-                                <el-input></el-input>
+                                <el-input v-model="salesInfo.projPreconAmt"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="订单类型">
-                            <el-select>
+                            <el-select  v-model="salesInfo.projOrdType">
                                 <el-option label="new sell" value="01"></el-option>
                                 <el-option label="Up sell" value="02"></el-option>
                                 <el-option label="Renew" value="03"></el-option>
@@ -79,7 +79,7 @@
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="项目交付地">
-                                <el-input></el-input>
+                                <el-input v-model="salesInfo.projImplePla"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
@@ -87,6 +87,7 @@
                             <el-date-picker
                                 type="date"
                                 placeholder="选择日期"
+                                v-model="salesInfo.projPreconDate"
                                 >
                             </el-date-picker>
                         </el-form-item>
@@ -96,13 +97,14 @@
                             <el-date-picker
                                 type="date"
                                 placeholder="选择日期"
+                                v-model="salesInfo.projPreincmDate"
                                 >
                             </el-date-picker>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="收入类型">
-                            <el-select>
+                            <el-select v-model="salesInfo.projIncmType">
                                 <el-option label="Pipeline" value="01"></el-option>
                                 <el-option label="EATP" value="02"></el-option>
                                 <el-option label="Frotlog" value="03"></el-option>
@@ -112,7 +114,7 @@
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="合同编号">
-                            <el-input readonly="readonly"></el-input>
+                            <el-input readonly="readonly" v-model="salesInfo.projConno"></el-input>
                             <el-upload class="upload-demo" ref="upload":show-file-list="false" :auto-upload="false" action="">
                                 <el-button slot="trigger" size="small" type="primary" class="uploadBtn">上传合同</el-button>
                             </el-upload>
@@ -120,7 +122,7 @@
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="项目状态">
-                            <el-select>
+                            <el-select v-model="salesInfo.projState">
                                 <el-option label="售前" value="01"></el-option>
                                 <el-option label="实施" value="02"></el-option>
                                 <el-option label="结束" value="03"></el-option>
@@ -131,39 +133,39 @@
                     <el-col :span="24" class="item-title">销售信息</el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="销售">
-                                <el-input></el-input>
+                                <el-input v-model="salesInfo.projSaleName"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="销售主管">
-                                <el-input></el-input>
+                                <el-input v-model="salesInfo.projSaleLinemgr"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :span="24">
                         <el-form-item label="项目说明">
-                            <el-input type="textarea"  class="b-textarea"></el-input>
+                            <el-input type="textarea"  class="b-textarea" v-model="salesInfo.remark"></el-input>
                         </el-form-item>
                     </el-col>
                     
                     <el-col :span="24" class="item-title">项目实施</el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="部门中心">
-                                <el-input></el-input>
+                                <el-input v-model="projImpleInfo.projImpDepname"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="部门编号">
-                            <el-input ></el-input>
+                            <el-input v-model="projImpleInfo.projImpDepno"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="成本中心">
-                            <el-input></el-input>
+                            <el-input v-model="projImpleInfo.projImpCcc"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="实施进度">
-                            <el-select>
+                            <el-select v-model="projImpleInfo.projImpStep">
                                 <el-option label="NEW" value="01"></el-option>
                                 <el-option label="需求分析" value="02"></el-option>
                                 <el-option label="设计阶段" value="03"></el-option>
@@ -177,30 +179,30 @@
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="项目经理">
-                            <el-input :disabled="true"></el-input>
+                            <el-input :disabled="true" v-model="projImpleInfo.projImpPm"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="项目主管">
-                                <el-input :disabled="true"></el-input>
+                                <el-input :disabled="true" v-model="projImpleInfo.projImpPm"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="计划开始时间">
-                                <el-input :disabled="true"></el-input>
+                                <el-input :disabled="true" v-model="projImpleInfo.projImpBegdate"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :sm="24" :md="12">
                         <el-form-item label="计划结束时间">
-                            <el-input :disabled="true"></el-input>
+                            <el-input :disabled="true" v-model="projImpleInfo.projImpEndate"></el-input>
                         </el-form-item>
                     </el-col>
                     <el-col :span="24">
                         <el-form-item label="项目说明">
-                            <el-input type="textarea" :disabled="true" class="b-textarea"></el-input>
+                            <el-input type="textarea" :disabled="true" class="b-textarea" v-model="projImpleInfo.projImpDesc"></el-input>
                         </el-form-item>
                     </el-col>
-                    <el-col :span="24" class="item-title">项目分包</el-col>
+                    <!-- <el-col :span="24" class="item-title">项目分包</el-col>
                     <el-col :span="24">
                         <el-table stripe :data="tableList" class="table-add">
                             <el-table-column align="center" prop="projImpDepno" label="分包合同号">
@@ -221,7 +223,7 @@
                                 </template>
                             </el-table-column>
                         </el-table>
-                    </el-col>
+                    </el-col> -->
                    
                 </el-form>
             </div>
@@ -231,7 +233,7 @@
 <script type='text/ecmascript-6'>
 	import current from "../../../common/current_position.vue";
     import api from "../../../../common/api/api.js"
-    // let { queryProjList } = api
+    let { queryProjAndSalesInfo,queryProjImpleInfo} = api
 	export default {
 		data() {
 			return{
@@ -254,6 +256,38 @@
                 pagination:{
                     pageSize:10,
                     total:20
+                },
+                salesInfo:{
+                    oppoNo:'1003223',
+                    projName:'项目名称',
+                    projNo:'项目编号',
+		            coocustNo:'客户编号',
+                    projSrvMngmode:'服务管理模式',
+                    projType:'项目类型',
+                    projIncmConfim:'收入确实类型',
+                    projPreconAmt:'预计合同金额',
+                    projOrdType:'订单类型:',
+                    projImplePla:'项目的交付地点:',
+                    projPreconDate:'2017-08-8',
+                    projPreincmDate:'2017-08-9',
+                    projIncmType:'收入类型',
+                    projConno:'合同编号',
+                    projState:'项目的状态',
+                    projSaleName:'销售',
+                    projSaleLinemgr:'销售的主管',
+                    remark:'项目说明'
+                },
+                projImpleInfo:{
+                    projImpDepname:'部门名称',
+                    projImpDepno:'部门编号',
+                    projImpCcc:'成本中心:',
+                    projImpStep:'实施进度',
+                    projImpPm:'项目经理',
+                    projImpMgr:'项目主管',
+		            projImpBegdate:'计划开始的时间',
+                    projImpEndate:'计划结束的时间',
+                    projImpDesc:'项目说明'
+		
                 }
             }
         },
