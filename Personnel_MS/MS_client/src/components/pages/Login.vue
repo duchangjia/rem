7b3272c51ede085a23044132f759073a2221547b
chenyongjian <80729453@qq.com>
2017-12-15 14:47:22 +0800

    1215save1

--- Personnel_MS/MS_client/src/components/pages/Login.vue
+++ Personnel_MS/MS_client/src/components/pages/Login.vue
@@ -6,7 +6,7 @@
                 <div class="img-wrapper"><img src="../../../static/img/common/omc_logo1.png" alt=""></div>
                 <div class="form-wrapper">
                     <img src="../../../static/img/login/fly.png" alt="" width="86" height="77" class="fly">
-                    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="form-content"  v-loading.fullscreen.lock="fullscreenLoading"  element-loading-text="登录中">
+                    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="form-content">
                         <span class="title">用户登录</span>
                         <el-form-item prop="username">
                             <el-input v-model="ruleForm.username" placeholder="请输入用户名"></el-input>
@@ -15,17 +15,18 @@
                             <el-input type="password" placeholder="请输入密码" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')" auto-complete="off"></el-input>
                         </el-form-item>
                         <el-form-item class="code-wrapper" prop="imageCode">
-                            <el-input  placeholder="请输入验证码" class="last-input" v-model="ruleForm.imageCode" @keyup.enter.native="submitForm('ruleForm')" @focus="errorMsg=''"></el-input><span class="check-code" @click="changeCode"><img
-                                :src="pic"></span>
+                            <el-input  placeholder="请输入验证码" class="last-input" v-model="ruleForm.imageCode" @keyup.enter.native="submitForm('ruleForm')" @focus="show=false"></el-input><span class="check-code" @click="changeCode"><img
+                                :src="pic" style="vertical-align: middle"></span>
+                            <transition name="slide">
+                                <span class="error" v-show="show">{{errorMsg}}</span>
+                            </transition>
                         </el-form-item>
-                        <div class="error" v-show="errorMsg">{{errorMsg}}</div>
                         <el-button class="tijiao" @click="submitForm('ruleForm')">登录</el-button>
                         <a class="text" href="#" @click="fegetPassword">忘记密码</a>
                     </el-form>
                     <div class="line1"></div>
                     <div class="line2"></div>
                     <div class="line3"></div>
-                    <!--<errtip :title="title" :content="content" class="errtip" ref="errtip"></errtip>-->
                 </div>
             </div>
             <div class="bottom">
@@ -33,31 +34,11 @@
                     橙色魔方版权所有 © Copyright Orange Magic Cube Co.ltd.All Rights Reserved. | 粤ICP备2553149号
                 </span>
             </div>
-        <!--<div class="ms-title">后台管理系统</div>-->
-        <!--<div class="ms-login">-->
-            <!--<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm">-->
-                <!--<el-form-item prop="username">-->
-                    <!--<el-input v-model="ruleForm.username" placeholder="username"></el-input>-->
-                <!--</el-form-item>-->
-                <!--<el-form-item prop="password">-->
-                    <!--<el-input type="password" placeholder="password" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')"></el-input>-->
-                <!--</el-form-item>-->
-                <!--<div class="login-btn">-->
-                    <!--<el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>-->
-                    <!--<p style="font-size:12px;line-height:30px;color:#999;">Tips : 用户名和密码随便填。</p>-->
-                <!--</div>-->
-            <!--</el-form>-->
-        <!--</div>-->
         </div>
-        <errtip></errtip>
-        <!--<toast></toast>-->
     </div>
 </template>
 
 <script>
-	import Bus from '../../common/Bus.js'
-//	import Toast from '../common/Toast.vue';
-	import errtip from './errTip.vue'
     export default {
         data: function(){
             var validateimageCode = (rule, value, callback) => {
@@ -70,7 +51,7 @@
                 }
             }
             return {
-                fullscreenLoading: false,
+                show:false,
                 pic:'',
                 errorMsg: '',
                 ruleForm: {
@@ -115,9 +96,6 @@
                         })
                 })
         },
-        components: {
-        	errtip
-        },
         methods: {
         	fegetPassword() {
 				this.$alert('如忘记密码，请联系系统管理人员进行密码重置操作。', '温馨提示', {
@@ -127,6 +105,7 @@
 		          }
 		        });
 			},
+            // 请求验证码
             changeCode() {
               this.$axios.get('/api/auth/code/image', {responseType: 'blob'})
                   .then( res => {
@@ -144,49 +123,26 @@
                 const self = this;
                 self.$refs[formName].validate((valid) => {
                     if (valid) {
-                        self.fullscreenLoading = true;
-//                        var loadingInstance = this.$loading({
-//                            target: '.form-content',
-//                            lock: true,
-//                            text: '加载中'
-//                        })
-                    	//吐丝提示
-//						Bus.$emit('showToast','正在努力加载中...');
                         var params = {
                             'username': self.ruleForm.username,
                             'password': self.ruleForm.password,
                             'imageCode': self.ruleForm.imageCode
                         };
-//                            'http://10.0.0.6:3000/checkuser'
-//                            self.$axios.get('/ifdp/checkuser',data)
-//                        dataType:"json",
-//                    contentType:"application/json",
-//                    data:JSON.stringify(this.content2),
-                            self.$axios.post('/api/auth/authentication/login', params, {
-                                auth:{
-                                    username:'iem_cloud',
-                                    password:'thisissecret'
-                                },
-                            })
-                                .then( function (res) {
-//                                    let result = res.data.data
+                        self.$axios.post('/api/auth/authentication/login', params, {
+                            auth:{
+                                username:'iem_cloud',
+                                password:'thisissecret'
+                            },
+                        }).then( function (res) {
                                     let access_token = res.data.access_token
                                     let refresh_token = res.data.refresh_token
                                     window.localStorage.setItem('access_token', access_token)
                                     window.localStorage.setItem('refresh_token', refresh_token)
-                                    if(true){
-                                        localStorage.setItem('ms_username',self.ruleForm.username);
-                                        self.fullscreenLoading = false
-                                        self.$router.push('aggPage');
-                                    }else{
-                                        Bus.$emit('showErrTip',{content:'登录失败！帐号或密码错。',title:'温馨提示'});
-//                                        self.content = '登录失败！帐号或密码错'
-//                                        self.$refs.errtip.errflag = false
-                                    }
-                                })
-                                .catch( function (e) {
-                                    self.fullscreenLoading = false
+                                    localStorage.setItem('ms_username',self.ruleForm.username);
+                                    self.$router.push('aggPage');
+                        }).catch( function (e) {
                                     self.errorMsg = e.content
+                                    self.show = true
                                     console.log(e)
                                     self.$axios.get('/api/auth/code/image', {responseType: 'blob'})
                                         .then( res => {
@@ -199,37 +155,15 @@
                                         .catch( (e)=> {
                                             console.log(e)
                                         })
-                                })
-                    } else {
-//                        self.content = '登录失败！请填写正确的账号和密码'
-//                        console.log(self.content,self.title,self.$refs.errtip)
-//                        self.$nextTick(function () {
-//                            self.$refs.errtip.errflag = false
-//                        })
-
-                        Bus.$emit('showErrTip',{content:'登录失败！请填写正确的账号和密码。',title:'温馨提示'});
-//                        this.$alert('请填写正确的账号和密码', '温馨提示', {
-//                            confirmButtonText: '确定',
-//                            callback: action => {
-//                                this.$message({
-//                                    type: 'info',
-//                                    message: `action: ${ action }`
-//                                });
-//                            }
-//                        });
-
+                        })
                     }
                 });
-                return false
             }
         }
     }
 </script>
 
 <style>
-    .el-loading-mask{
-        background-color: rgba(0,0,0,.5);
-    }
     #wrap .login-wrap{
         overflow: hidden;
         width: 100%;
@@ -329,22 +263,13 @@
         font-size: 14px;
         color: #333333;
     }
-    /*#wrap .form-content .el-form-item:nth-child(odd){*/
-        /*letter-spacing:  -0.34px;*/
-    /*}*/
-    #wrap .el-form-item .el-input{
-        /*width: 260px;*/
-        /*height: 40px;*/
-    }
     #wrap .el-form-item .el-input .el-input__inner{
         width: 260px;
         height: 40px;
     }
     #wrap .form-content .code-wrapper{
-        /*margin: 0 auto 23px auto;*/
         margin-bottom: 23px;
         box-sizing: border-box;
-        /*width: 260px;*/
     }
     #wrap .form-content .last-input{
         width: 160px;
@@ -354,37 +279,50 @@
     #wrap .form-content .last-input .el-input__inner{
         width: 160px;
         height: 40px;
-        /*border: 1px solid #EEEEEE;*/
-        /*border-radius: 4px;*/
         font-family: PingFangSC-Regular;
-        /*font-size: 14px;*/
         color: #333333;
-        /*letter-spacing: -0.34px;*/
-        /*line-height: 14px;*/
     }
     #wrap .form-content .check-code{
         background: #F9F9F9;
         width: 90px;
         height: 40px;
         display: inline-block;
-        /*vertical-align: middle;*/
         text-align: center;
         line-height: 40px;
-        /*font-family: PingFangSC-Regular;*/
-        /*font-size: 18px;*/
-        /*color: #282828;*/
-        /*letter-spacing: 4px;*/
     }
     #wrap .form-content .error{
-        margin: -23px auto 10px 35px;
-        font-family: PingFangSC-Regular;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        height: 16px;
+        color: #ff4949;
         font-size: 12px;
-        color: #FF6666;
-        letter-spacing: -0.29px;
-        height: 14px;
-        line-height: 14px;
-        /*transform: translateY(-23px);*/
-        /*animation: slidedown .2s;*/
+        line-height: 1;
+        padding-top: 4px;
+        overflow: hidden;
+    }
+    .slide-enter-active {
+        animation: bounce-in .5s;
+    }
+    .slide-leave-active {
+        animation: bounce-out .5s ;
+    }
+    @keyframes bounce-in {
+        0% {
+            height: 0;
+        }
+
+        100% {
+            height: 16px;
+        }
+    }
+    @keyframes bounce-out {
+        0% {
+            height: 16px;
+        }
+        100% {
+            height: 0;
+        }
     }
     #wrap .form-content .tijiao{
         display: block;
