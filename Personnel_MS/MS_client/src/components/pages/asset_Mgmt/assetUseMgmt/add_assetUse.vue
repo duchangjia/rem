b6597b7eac074429cc6f1d69211bbf3d9c036382
fqlnet <fangqlnet@163.com>
2017-12-25 18:39:07 +0800

    资产使用新增

--- Personnel_MS/MS_client/src/components/pages/asset_Mgmt/assetUseMgmt/add_assetUse.vue
+++ Personnel_MS/MS_client/src/components/pages/asset_Mgmt/assetUseMgmt/add_assetUse.vue
@@ -10,23 +10,20 @@
                     <el-form  :model="info" :rules="rules" :inline="true" ref="info1" label-width="130px"  label-position="right" >
                         <el-col :sm="24" :md="12" >
                             <el-form-item label="公司名称">
-                                <el-select placeholder="请选择公司名称" :disabled="true" v-model="applyUserInfo.organName">
-                                    <el-option label="区域一" value="shanghai"></el-option>
-                                </el-select>
+                                <el-input :disabled="true" v-model="applyUserInfo.organName">
+                                </el-input>
                             </el-form-item>
                         </el-col>
                         <el-col :sm="24" :md="12" >
                             <el-form-item label="申请部门名称">
-                                <el-select placeholder="请选择申请部门名称" :disabled="true" v-model="applyUserInfo.derpName">
-                                    <el-option label="区域一" value="shanghai"></el-option>
-                                </el-select>
+                                <el-input :disabled="true" v-model="applyUserInfo.derpName">
+                                </el-input>
                             </el-form-item>
                         </el-col>
                         <el-col :sm="24" :md="12" >
                             <el-form-item label="CCC">
-                                <el-select placeholder="请选择CCC" :disabled="true" v-model="applyUserInfo.ccc">
-                                    <el-option label="区域一" value="shanghai"></el-option>
-                                </el-select>
+                                <el-input :disabled="true" v-model="applyUserInfo.ccc">
+                                </el-input>
                             </el-form-item>
                         </el-col>
                         <el-col :sm="24" :md="12" >
@@ -59,12 +56,16 @@
                         </el-col>
                         <el-col :sm="24" :md="12">
                             <el-form-item label="岗位">
-                                <el-input :disabled="true" v-model="applyUserInfo.custPost"></el-input>
+                                <el-select :disabled="true" v-model="applyUserInfo.custPost">
+                                    <el-option v-for="item in custPostList" :key="item.paraValue" :label="item.paraShowMsg" :value="item.paraValue"></el-option>
+                                </el-select>
                             </el-form-item>
                         </el-col>
                          <el-col :sm="24" :md="12">
                             <el-form-item label="职级">
-                                <el-input :disabled="true" v-model="applyUserInfo.custClass"></el-input>
+                                <el-select :disabled="true" v-model="applyUserInfo.custClass">
+                                    <el-option v-for="item in custClassList" :key="item.paraValue" :label="item.paraShowMsg" :value="item.paraValue"></el-option>
+                                </el-select>
                             </el-form-item>
                          </el-col>
                     </el-form>
@@ -193,11 +194,11 @@
 </template>
 
 <script type='text/ecmascript-6'>
-import current from "../../../common/current_position.vue"
-import messageBox from "../../../common/messageBox-components.vue"
-import moment from "moment"
-import api from '../../../../common/api/api.js'
-let {addAssetUseINF} = api
+import current from "../../../common/current_position.vue";
+import messageBox from "../../../common/messageBox-components.vue";
+import moment from "moment";
+import api from "../../../../common/api/api.js";
+let { addAssetUseINF } = api;
 export default {
   data() {
     return {
@@ -208,8 +209,13 @@ export default {
         assetNo: [{ required: true, message: "请选择资产编号", trigger: "change" }],
         applyType: [{ required: true, message: "请选择使用类别", trigger: "change" }],
         applyNum: [
-            { pattern: /^(0|([1-9][0-9]{0,10}))$/, message: "请输入正整数",trigger: "blur",required: true }
-            ],
+          {
+            pattern: /^(0|([1-9][0-9]{0,10}))$/,
+            message: "请输入正整数",
+            trigger: "blur",
+            required: true
+          }
+        ],
         applyTime: [{ required: true, message: "请输入发生时间", trigger: "blur" }],
         remark: [{ required: true, message: "请输入说明", trigger: "blur" }],
         applyStatus: [{ required: true, message: "请选择状态", trigger: "change" }]
@@ -218,70 +224,82 @@ export default {
         applyUserNo: "",
         assetNo: ""
       },
-    tableOption:[],
-    dialogVisible:false,
-    inputFirstOption:{},
-    inputSecOption:{},
-    msgPagination:{},
-    searchData:{},
-    searchUrl:'',
-    saveUrl:'',
-    boxTitle:'',
-    numType:'',
-    applyUserInfo: {},
-    assetInfo: {},
-    applyInfo: {
+      tableOption: [],
+      dialogVisible: false,
+      inputFirstOption: {},
+      inputSecOption: {},
+      msgPagination: {},
+      searchData: {},
+      searchUrl: "",
+      saveUrl: "",
+      boxTitle: "",
+      numType: "",
+      applyUserInfo: {},
+      assetInfo: {},
+      applyInfo: {
         remark: "",
         applyStatus: "",
         applyTime: "",
         applyNum: "",
         applyType: ""
-      }
+      },
+      custPostList: [],
+      custClassList: []
     };
   },
+  created() {
+    this.getCustPostList(); //查询岗位列表
+    this.getCustClassList(); //查询职级列表
+  },
   methods: {
-    dialogConfirm(ajaxNo){
-        let self = this;
-        self.dialogVisible = false;
-        self.$axios
-        .get(
-          self.saveUrl+
-          ajaxNo.stateNo
-        )
+    dialogConfirm(ajaxNo) {
+      let self = this;
+      self.dialogVisible = false;
+      self.$axios
+        .get(self.saveUrl + ajaxNo.stateNo)
         .then(res => {
-          if (res.data.code == 'F00002'){
+          if (res.data.code == "F00002") {
             self.$message({
-              message:res.data.retMsg,
+              message: res.data.retMsg,
               type: "error"
             });
-          }else{
-            switch(self.numType){
-                case 1:
-                    self.applyUserInfo = res.data.data
-                    self.info.applyUserNo = ajaxNo.stateNo
+          } else {
+            switch (self.numType) {
+              case 1:
+                self.applyUserInfo = res.data.data;
+                self.info.applyUserNo = ajaxNo.stateNo;
+                switch (self.applyUserInfo.ccc) {
+                  case "01":
+                    self.applyUserInfo.ccc = "管理CCC";
+                    break;
+                  case "02":
+                    self.applyUserInfo.ccc = "售前CCC";
+                    break;
+                  case "03":
+                    self.applyUserInfo.ccc = "项目CCC";
+                    break;
+                }
                 break;
-                case 2:
-                    
-                    self.assetInfo = res.data.data
-                    self.info.assetNo = ajaxNo.stateNo
-                    switch(self.assetInfo.assetType){
-                         case "01":
-                            self.assetInfo.assetType = "办公用品";
-                            break;
-                        case "02":
-                            self.assetInfo.assetType = "电脑";
-                            break;
-                        case "03":
-                            self.assetInfo.assetType = "手机";
-                            break;
-                        case "04":
-                            self.assetInfo.assetType = "后勤用品";
-                            break;
-                        case "05":
-                            self.assetInfo.assetType = "数码相机";
-                            break;
-                    }
-                    
+              case 2:
+                self.assetInfo = res.data.data;
+                self.info.assetNo = ajaxNo.stateNo;
+                switch (self.assetInfo.assetType) {
+                  case "01":
+                    self.assetInfo.assetType = "办公用品";
+                    break;
+                  case "02":
+                    self.assetInfo.assetType = "电脑";
+                    break;
+                  case "03":
+                    self.assetInfo.assetType = "手机";
+                    break;
+                  case "04":
+                    self.assetInfo.assetType = "后勤用品";
+                    break;
+                  case "05":
+                    self.assetInfo.assetType = "数码相机";
+                    break;
+                }
                 break;
             }
           }
@@ -289,98 +307,124 @@ export default {
         .catch(e => {
           self.applyUserInfo = {};
           self.$message({
-            message:e.retMsg,
+            message: e.retMsg,
             type: "error"
           });
         });
     },
-    userNoSelect(){
-        //table
-        this.tableOption = [
-            {
-                thName:'工号',//table 表头
-                dataKey:'userNo'//table-col所绑定的prop值
-            },
-            {
-                thName:'姓名',//table 表头
-                dataKey:'custName'//table-col所绑定的prop值
-            }
-            ];
-        //input 第一个搜索框的配置项
-        this.inputFirstOption  = {
-            labelName:'姓名',//label头
-            placeholder:'请输入姓名'//input placeholder
+    userNoSelect() {
+      //table
+      this.tableOption = [
+        {
+          thName: "工号", //table 表头
+          dataKey: "userNo" //table-col所绑定的prop值
         },
+        {
+          thName: "姓名", //table 表头
+          dataKey: "custName" //table-col所绑定的prop值
+        }
+      ];
+      //input 第一个搜索框的配置项
+      (this.inputFirstOption = {
+        labelName: "姓名", //label头
+        placeholder: "请输入姓名" //input placeholder
+      }),
         //input 第二个搜索框的配置项
-        this.inputSecOption  = {
-            labelName:'工号',
-            placeholder:'请输入工号'
-        },
+        (this.inputSecOption = {
+          labelName: "工号",
+          placeholder: "请输入工号"
+        }),
         //搜索所需传值
-        this.searchData = {
-            custName:'',
-            userNo:''
-        }
-        //table分页所需传值
-        this.msgPagination =  {
-            pageNum:1,
-            pageSize:5,
-            totalRows:0
-        }
-        //点击确定后需要修改的对象 numType为changeNo方法所改变的type
-        this.numType = 1
-        //dialog打开
-        this.dialogVisible=true
-        //查询接口
-        this.searchUrl = "/iem_hrm/CustInfo/queryCustBasicInfList"
-        //点击确定后根据号码查询用户信息借口 没有则为空
-        this.saveUrl = '/iem_hrm/assetUse/queryAssetUserByApplyUserNo/'
-        //dialog标题
-        this.boxTitle = '人工编号选择'
+        (this.searchData = {
+          custName: "",
+          userNo: ""
+        });
+      //table分页所需传值
+      this.msgPagination = {
+        pageNum: 1,
+        pageSize: 5,
+        totalRows: 0
+      };
+      //点击确定后需要修改的对象 numType为changeNo方法所改变的type
+      this.numType = 1;
+      //dialog打开
+      this.dialogVisible = true;
+      //查询接口
+      this.searchUrl = "/iem_hrm/CustInfo/queryCustBasicInfList";
+      //点击确定后根据号码查询用户信息借口 没有则为空
+      this.saveUrl = "/iem_hrm/assetUse/queryAssetUserByApplyUserNo/";
+      //dialog标题
+      this.boxTitle = "人工编号选择";
     },
-    assetNoSelect(){
-          //table
-        this.tableOption = [
-            {
-                thName:'资产编号',//table 表头
-                dataKey:'assetNo'//table-col所绑定的prop值
-            },
-            {
-                thName:'资产名称',//table 表头
-                dataKey:'assetName'//table-col所绑定的prop值
-            }
-            ];
-        //input 第一个搜索框的配置项
-        this.inputFirstOption  = {
-            labelName:'资产名称',//label头
-            placeholder:'请输入资产名称'//input placeholder
+    assetNoSelect() {
+      //table
+      this.tableOption = [
+        {
+          thName: "资产编号", //table 表头
+          dataKey: "assetNo" //table-col所绑定的prop值
         },
+        {
+          thName: "资产名称", //table 表头
+          dataKey: "assetName" //table-col所绑定的prop值
+        }
+      ];
+      //input 第一个搜索框的配置项
+      (this.inputFirstOption = {
+        labelName: "资产名称", //label头
+        placeholder: "请输入资产名称" //input placeholder
+      }),
         //input 第二个搜索框的配置项
-        this.inputSecOption  = {
-            labelName:'资产编号',
-            placeholder:'请输入资产编号'
-        },
+        (this.inputSecOption = {
+          labelName: "资产编号",
+          placeholder: "请输入资产编号"
+        }),
         //搜索所需传值
-        this.searchData = {
-            assetName:'hahha',
-            assetNo:''
-        }
-        //table分页所需传值
-        this.msgPagination =  {
-            pageNum:1,
-            pageSize:5,
-            totalRows:0
-        }
-        //点击确定后需要修改的对象 numType为changeNo方法所改变的type
-        this.numType = 2
-        //dialog打开
-        this.dialogVisible=true
-        //查询接口
-        this.searchUrl = "/iem_hrm/assetUse/queryAssetNoList"
-        //点击确定后根据号码查询用户信息借口 没有则为空
-        this.saveUrl = '/iem_hrm/assetUse/queryAssetUserByAssetNo/'
-        //dialog标题
-        this.boxTitle = '资产编号选择'
+        (this.searchData = {
+          assetName: "hahha",
+          assetNo: ""
+        });
+      //table分页所需传值
+      this.msgPagination = {
+        pageNum: 1,
+        pageSize: 5,
+        totalRows: 0
+      };
+      //点击确定后需要修改的对象 numType为changeNo方法所改变的type
+      this.numType = 2;
+      //dialog打开
+      this.dialogVisible = true;
+      //查询接口
+      this.searchUrl = "/iem_hrm/assetUse/queryAssetNoList";
+      //点击确定后根据号码查询用户信息借口 没有则为空
+      this.saveUrl = "/iem_hrm/assetUse/queryAssetUserByAssetNo/";
+      //dialog标题
+      this.boxTitle = "资产编号选择";
+    },
+    getCustPostList() {
+      let self = this;
+      self.$axios
+        .get("/iem_hrm/sysParamMgmt/queryPubAppParams?paraCode=CUST_POST")
+        .then(res => {
+          if (res.data.code === "S00000") {
+            self.custPostList = res.data.data;
+          }
+        })
+        .catch(err => {
+          console.log("error");
+        });
+    },
+    getCustClassList() {
+      let self = this;
+      self.$axios
+        .get("/iem_hrm/sysParamMgmt/queryPubAppParams?paraCode=PER_ENDM_FIXED")
+        .then(res => {
+          if (res.data.code === "S00000") {
+            self.custClassList = res.data.data;
+          }
+        })
+        .catch(err => {
+          console.log("error");
+        });
     },
     save() {
       let self = this;
@@ -391,29 +435,28 @@ export default {
               self.$refs.info3.validate(valid => {
                 if (valid) {
                   let data = Object.assign(this.applyInfo, this.info);
-                      self.$axios
-                        .post(addAssetUseINF, data)
-                        .then(res => {
-                          let result = res.data.retMsg;
-                          if ("操作成功" === result) {
-                            self.$message({
-                              message:result,
-                              type: "success"
-                            });
-                          } else {
-                            self.$message({
-                              message: result,
-                              type: "error"
-                            });
-                          }
-                        })
-                        .catch(e => {
-                          self.$message({
-                              message: e.retMsg,
-                              type: "error"
-                            });
+                  self.$axios
+                    .post(addAssetUseINF, data)
+                    .then(res => {
+                      let result = res.data.retMsg;
+                      if ("操作成功" === result) {
+                        self.$message({
+                          message: result,
+                          type: "success"
                         });
-                   
+                      } else {
+                        self.$message({
+                          message: result,
+                          type: "error"
+                        });
+                      }
+                    })
+                    .catch(e => {
+                      self.$message({
+                        message: e.retMsg,
+                        type: "error"
+                      });
+                    });
                 }
               });
             }
@@ -421,16 +464,14 @@ export default {
         }
       });
     },
-   
-    changeDate(){
-       let applyTime =  moment(this.applyInfo.applyTime).format("YYYY-MM-DD");
-       this.applyInfo.applyTime = applyTime.toString();
-        console.log(applyTime);
+
+    changeDate() {
+      let applyTime = moment(this.applyInfo.applyTime).format("YYYY-MM-DD");
+      this.applyInfo.applyTime = applyTime.toString();
+      console.log(applyTime);
     }
   },
-  filters:{
-    
-  },
+  filters: {},
   components: {
     current,
     messageBox
